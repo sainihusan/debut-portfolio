3a629bd330923697d20eb7678d7fbd932efc490b
Husanpreet Singh <167855282+sainihusan@users.noreply.github.com>
2025-11-14 10:18:05 +0530

    project card responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
-import { Box, CssBaseline, } from "@mui/material";
+import { Box, CssBaseline } from "@mui/material";
 import Work from "./Components/work";
 import ProjectCard from "./Components/Projectcard";
 import ExpertiseSection from "./Components/Expertisesection";
+
 
 import ifincaBg from "./assets/ifinca-bg.webp";
 import ifinca from "./assets/ifinca.webp";
 import integraBg from "./assets/integra-bg.webp";
 import friendspireBg from "./assets/friendspire-bg.webp";
-import nordicStrong from "./assets/Nordic-strong.webp";
 import uniqueSchool from "./assets/uniqueschool-bg.webp";
 import cimet from "./assets/cimet-bg.webp";
+
+import nordicStrong from "./assets/Nordic-strong.webp";
+import uniqueSchoolBg from "./assets/uniqueschool-bg.webp";
 import nordicLogo from "./assets/nordic-strong-logo.webp";
 import propawn from "./assets/propawn.webp";
 import propawnLogo from "./assets/propawn-logo.webp";
-import skyLogo from "./assets/logo.png";
 import silverSky from "./assets/silverskysoft.webp";
+import skyLogo from "./assets/logo.png";
 import tem from "./assets/tem.webp";
 import temLogo from "./assets/tem-logo.webp";
 import collectionLogo from "./assets/collection-logo.webp";
 import collectionaries from "./assets/collectionaries.webp";
-import openkey from "./assets/openkey.webp"
-import openkeyLogo from "./assets/openkey-logo.webp"
-import hdfcLogo from "./assets/hdfc-logo.png"
-import hdfc from "./assets/hdfc.webp"
-import kfcLogo from "./assets/kfc-logo.png"
-import kfc from "./assets/kfc.webp"
-
+import openkey from "./assets/openkey.webp";
+import openkeyLogo from "./assets/openkey-logo.webp";
+import hdfcLogo from "./assets/hdfc-logo.png";
+import hdfc from "./assets/hdfc.webp";
+import kfcLogo from "./assets/kfc-logo.png";
+import kfc from "./assets/kfc.webp";
 
 export default function App() {
   const projects = [
@@ -34,17 +36,17 @@ export default function App() {
       bgImage: ifincaBg,
       overlayImage: ifinca,
       bgColor: "#317062",
-      subtitle:
-        "A blockchain based solution to streamline the coffee supply chain",
+      subtitle: "A blockchain based solution to streamline the coffee supply chain",
       description:
-        "An innovative solution that optimizes and simplifies the coffee supply chain, ensuring transparency, efficiency, and sustainability from farm to cup.",
+        "An innovative solution that optimizes and simplifies the coffee supply chain, ensuring transparency and efficiency.",
       points: [
         "Received seed funding of $2 million",
-        "Recognized by Coffee Association Boston USA and Start-up Columbia",
-        "Over 10,000 stakeholders currently using the platform",
+        "Recognized by Coffee Association Boston USA",
+        "10,000+ stakeholders currently using the platform",
       ],
       tags: ["Supply chain", "Hyperledger Fabric", "Blockchain"],
     },
+
     {
       title: "Integra Ledger",
       bgImage: integraBg,
@@ -52,56 +54,59 @@ export default function App() {
       bgColor: "#182F63",
       subtitle: "A Blockchain-based Digital Signature Platform",
       description:
-        "A comprehensive blockchain-based solution for secure and efficient data sharing that ensures tamper-proof records, enabling seamless collaboration and transparency among legal professionals.",
+        "A secure digital signature platform ensuring tamper-proof legal documentation.",
       points: [
-        "Secured $3 million funding from the Capital Group",
-        "Used by over 300 law firms across the globe",
-        "Used by big enterprises like Seven Eleven, Mohawk oil, Hogan Lovell, etc.",
+        "Secured $3 million funding",
+        "Used by 300+ law firms",
+        "Trusted by global enterprises",
       ],
-      tags: ["Legal consortium", "Hyperledger Blockchain", "Web platform"],
+      tags: ["Legal", "Blockchain", "Secure Data Sharing"],
     },
+
     {
       title: "Friendspire",
       bgImage: friendspireBg,
       overlayImage: ifinca,
       bgColor: "#653fee",
-      subtitle: "A Personalized Entertainment Recommendation Solution",
+      subtitle: "Entertainment Recommendation App",
       description:
-        "A dynamic solution to enhance the way people discover and share recommendations with friends which empowers them to curate personalized lists of their favorite books, movies, TV shows, podcasts, and more.",
+        "A platform to discover books, movies, shows and share lists with friends.",
       points: [
-        "Secured seed funding of $300k",
-        "Got 10,000 registrations post-launch",
-        "Featured in CBS19, CNN & EU-Startups",
+        "Raised $300k seed",
+        "10,000 users post launch",
+        "Featured on CNN & EU-Startups",
       ],
-      tags: ["Social networking", "iPhone", "Android"],
+      tags: ["Social", "iPhone", "Android"],
     },
+
     {
       title: "Unique School",
       bgImage: uniqueSchool,
       overlayImage: ifinca,
       bgColor: "#173f58",
-      subtitle: "A Smart School Management Platform",
+      subtitle: "Smart School Management Platform",
       description:
-        "A smart school management platform that streamlines administrative tasks, enhances communication, collaboration, and addresses security concerns.",
+        "Helps schools automate admin, communication and security.",
       points: [
-        "Used by over 140 renowned schools across the UK and Ireland",
-        "More than 100,000 users currently using this platform",
-        "Funded by some of the world's leading venture investors",
+        "Used by 140+ schools",
+        "100,000+ users",
+        "Backed by top investors",
       ],
       tags: ["Education", "Web platform", "Mobile apps"],
     },
+
     {
       title: "Cimet",
       bgImage: cimet,
       overlayImage: ifinca,
       bgColor: "#1d4578",
-      subtitle: "Simplifying Decision-making Process Through Effortless Comparison",
+      subtitle: "Effortless Utility Comparison",
       description:
-        "Cimet is a powerful comparison platform that empowers users to make informed choices by providing comprehensive comparisons of various utilities.",
+        "A powerful comparison platform for energy, internet and utilities.",
       points: [
-        "Received $26.6 million funding from iSelect",
-        "Cimet has witnessed 130 percent annual growth since its launch",
-        "Recorded $11 million revenue in 2021-2022",
+        "$26.6M funding from iSelect",
+        "130% annual growth",
+        "Generated $11M revenue",
       ],
       tags: ["Web Development", "Energy Retail", "Utility Comparison"],
     },
@@ -113,72 +118,71 @@ export default function App() {
       logo: nordicLogo,
       title: "Nordic Strong",
       subtitle: "BOUTIQUE FITNESS APP",
-      description: "With the aim of delivering a convenient pawnshop experience, Propawn allows customers to explore and buy a wide range of items while accessing collateral-based loans."
+      description:
+        "A complete fitness app offering high-quality workout tracking and coaching.",
     },
     {
       bgImage: propawn,
       logo: propawnLogo,
       title: "Propawn",
-      subtitle:
-        "DIGITAL PAWNSHOP",
-      description: "With the aim of delivering a convenient pawnshop experience, Propawn allows customers to explore and buy a wide range of items while accessing collateral-based loans."
+      subtitle: "DIGITAL PAWNSHOP",
+      description: "A modern pawnshop experience for buying and loaning items.",
     },
     {
-      logo: skyLogo,
       bgImage: silverSky,
+      logo: skyLogo,
       title: "Blockchain Integration",
-      subtitle:
-        "APPOINTMENT BOOKING FOR SALONS",
-      description: "With its user-friendly interface and efficient scheduling tools, it optimizes operations for salon owners while providing a seamless and personalized booking experience for clients."
+      subtitle: "SALON BOOKING SYSTEM",
+      description:
+        "A smart appointment booking platform for salons and clients.",
     },
     {
-      logo: temLogo,
       bgImage: tem,
+      logo: temLogo,
       title: "Tem",
-      subtitle:
-        "HEALTH IMPROVEMENT APP",
-      description: "TEM provides personalized tools and resources for tracking fitness activities, monitoring nutrition, managing stress, and fostering healthy habits."
+      subtitle: "HEALTH IMPROVEMENT APP",
+      description:
+        "Helps users track fitness, nutrition, stress, and healthy habits.",
     },
     {
-      logo: collectionLogo,
       bgImage: collectionaries,
+      logo: collectionLogo,
       title: "Collectionaire",
-      subtitle:
-        "MEMORIES COLLECTION PLATFORM",
-      description: "Collectionaire is an online platform that simplifies and organizes personal collections. It allows users to digitally catalog and preserve valuable items, memories, and documents"
+      subtitle: "MEMORY COLLECTION PLATFORM",
+      description:
+        "A digital platform to preserve and organize memories and documents.",
     },
     {
-      logo: openkeyLogo,
       bgImage: openkey,
+      logo: openkeyLogo,
       title: "OpenKey",
-      subtitle:
-        "DIGITAL KEY SOLUTION FOR HOTELS",
-      description: "OpenKey is a hotel access management solution. It enables hotels to provide digital keys to guests through their smartphones, enhancing convenience and security."
+      subtitle: "DIGITAL HOTEL KEY",
+      description:
+        "Smart key solution for hotels offering seamless and secure access.",
     },
     {
-      logo: hdfcLogo,
       bgImage: hdfc,
+      logo: hdfcLogo,
       title: "HDFC Life Insta Sales",
-      subtitle:
-        "DIGITAL INSURANCE APP",
-      description: "The HDFC Life Insta simplifies the insurance sales process, providing a seamless digital platform to showcase products, calculate premiums, and facilitate customer interactions."
+      subtitle: "DIGITAL INSURANCE APP",
+      description:
+        "Simplifies insurance sales with premium calculation and product showcase.",
     },
     {
-      logo: kfcLogo,
       bgImage: kfc,
-      title: "KFC Delivery Store Application",
-      subtitle:
-        "FOOD RESTAURANT",
-      description: "The KFC delivery store app enhances operational efficiency and streamlines order management, ensuring smooth operations and customer satisfaction for the KFC business."
+      logo: kfcLogo,
+      title: "KFC Delivery Store App",
+      subtitle: "FOOD RESTAURANT",
+      description:
+        "Helps KFC teams manage orders smoothly and improve efficiency.",
     },
-
   ];
 
   return (
     <>
       <CssBaseline />
-
       <Work />
+
       {projects.map((project, index) => (
         <ProjectCard
           key={index}
@@ -186,9 +190,8 @@ export default function App() {
           direction={index % 2 === 0 ? "image-left" : "image-right"}
         />
       ))}
+
       <ExpertiseSection items={expertiseItems} />
-
-
     </>
   );
 }
--- src/Components/Projectcard.jsx
+++ src/Components/Projectcard.jsx
@@ -1,206 +1,155 @@
 import { Box, Typography, Chip, Button, Stack, Container } from "@mui/material";
 
 export default function Projectcard({ project, direction }) {
+    const isRight = direction === "image-left";
+
     return (
-        <Box
-            sx={{
-                display: "flex",
-                flexDirection: {
-                    xs: "column",
-                    md: direction === "image-right" ? "row-reverse" : "row",
-                },
-                alignItems: "center",
-                justifyContent: "space-between",
-                backgroundColor: project.bgColor,
-                color: "white",
-                position: "relative",
-                overflow: "hidden",
-                textAlign: { xs: "start", md: "left" },
-            }}
-        >
-            <Box
-                sx={{
-                    flex: 1,
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: { xs: "start", },
-                    position: "relative",
-                    left:
-                        direction === "image-right"
-                            ? { xs: "0px", md: "60px" }
-                            : { xs: "0px", md: "30px", },
-
-                    zIndex: 2,
-
-                }}
-            >
-                <Container maxWidth="md"
-                    sx={{
-                        px: { md: 0, sm: 3, },
-                    }}>
-
-                    <Typography
-                        sx={{
-                            fontWeight: "bold",
-                            mb: 2,
-                            mt: { xs: 3, md: 0 },
-                            fontSize: { xs: "2rem", md: "2.5rem" },
-                        }}
-                    >
-                        {project.title}
-                    </Typography>
-
-                    <Typography
-                        variant="h6"
-                        sx={{
-                            fontWeight: 600,
-                            lineHeight: 1.5,
-                            fontSize: { xs: "1.1rem", md: "19px" },
-                            width: { xs: "100%", md: "90%" },
-                        }}
-                    >
-                        {project.subtitle}
-                    </Typography>
-
-                    <Typography
-                        sx={{
-                            lineHeight: 1.7,
-                            fontSize: { xs: "1rem", md: "17px" },
-                            width: { xs: "100%", md: "90%" },
-                            mb: 2,
-                        }}
-                    >
-                        {project.description}
-                    </Typography>
-
-                    <Box
-                        component="ul"
-                        sx={{
-                            pl: 0,
-                            ml: { xs: 0, md: -2 },
-                            listStylePosition: "inside",
-                            fontSize: { xs: "0.85rem", md: "13px", lg: "17px" },
-                            lineHeight: 2,
-                            "& li": { marginBottom: "8px" },
-                            textAlign: { xs: "start", md: "left" },
-                        }}
-                    >
-                        {project.points.map((point, index) => (
-                            <li key={index}>{point}</li>
-                        ))}
-                    </Box>
-
-                    <Stack
-                        direction="row"
-                        spacing={1}
-                        useFlexGap
-                        flexWrap="wrap"
-                        sx={{
-                            mb: 4,
-                            justifyContent: { xs: "flex-start", md: "flex-start" },
-                            fontSize: { xs: "0.8rem", sm: "0.9rem", md: "1rem" },
-                            rowGap: 1,
-                        }}
-                    >
-                        {project.tags.map((tag, index) => (
-                            <Chip
-                                key={index}
-                                label={tag}
-                                sx={{
-                                    backgroundColor: "rgba(255, 255, 255, 0.15)",
-                                    color: "white",
-                                    fontSize: { xs: "0.7rem", sm: "0.85rem", md: "0.75rem", lg: "1rem" },
-                                    px: { xs: 0.5, sm: 0.4 },
-                                    py: { xs: 0.2, sm: 0.3 },
-                                    borderRadius: "8px",
-
-                                    "&:hover": {
-                                        backgroundColor: "rgba(255, 255, 255, 0.3)",
-                                    },
-                                    transition: "background-color 0.3s ease",
-                                }}
-                            />
-                        ))}
-                    </Stack>
-
-
-                    <Button
-                        variant="outlined"
-                        sx={{
-                            borderColor: "white",
-                            color: "white",
-                            borderRadius: "10px",
-                            px: 3,
-                            py: 1,
-                            textTransform: "none",
-                            fontWeight: "bold",
-                            fontSize: { xs: "0.9rem", md: "1rem" },
-                            alignSelf: { xs: "start", md: "flex-start" },
-                            mb: 2,
-                            "&:hover": {
-                                borderColor: "#fff",
-                                backgroundColor: "rgba(255,255,255,0.1)",
-                            },
-                        }}
-                    >
-                        View Case Study
-                    </Button>
-                </Container>
-
-
-                <Box
-                    component="img"
-                    src={project.overlayImage}
-                    alt={`${project.projectTitle} overlay`}
-                    sx={{
-                        display: { xs: "block", md: "none" },
-                        mt: 2,
-                        width: { xs: "220px" },
-                        height: "auto",
-                        alignSelf: "center",
-                    }}
-                />
-
-            </Box>
-
+        <Box sx={{ backgroundColor: project.bgColor, position: "relative", overflow: "hidden" }}>
             <Box
                 component="img"
                 src={project.bgImage}
-                alt={`${project.projectTitle} background`}
+                alt="bg"
                 sx={{
-                    flex: 1,
-
-                    justifyContent: "center",
-                    alignItems: "center",
-                    overflow: "hidden",
-                    position: "relative",
-                    width: "118%",
-                    height: "694px",
+                    position: "absolute",
+                    top: 0,
+                    [isRight ? "right" : "left"]: 0,
+                    width: "50%",
+                    height: "100%",
                     objectFit: "cover",
                     display: { xs: "none", md: "block" },
-
+                    zIndex: 1,
                 }}
-            >
-            </Box>
+            />
+
+            <Container maxWidth="xl">
+                <Box
+                    sx={{
+                        position: "relative",
+                        zIndex: 2,
+                        display: "flex",
+                        flexDirection: {
+                            xs: "column",
+                            md: isRight ? "row" : "row-reverse",
+                        },
+                        alignItems: "center",
+                        py: 3,
+                        gap: { xs: 1, md: 6 },
+                        mb: { xs: 1, lg: 6 },
+                        mt: 4
+                    }}
+                >
+                    <Box
+                        sx={{
+                            width: { xs: "100%", md: "50%" },
+                            display: "flex",
+                            flexDirection: "column",
+                            color: "white",
+                            textAlign: "left",
+                            pl: { md: isRight ? "0px" : "50px" },
+                            pr: { md: isRight ? "0px" : "50px" },
+                        }}
+                    >
+                        <Typography
+                            sx={{
+                                fontWeight: "bold",
+                                mb: 2,
+                                fontSize: { xs: "2rem", md: "2.5rem" },
+                            }}
+                        >
+                            {project.title}
+                        </Typography>
+
+                        <Typography
+                            variant="h6"
+                            sx={{
+                                fontWeight: 600,
+                                mb: 2,
+                                fontSize: { xs: "1.1rem", md: "19px" },
+                            }}
+                        >
+                            {project.subtitle}
+                        </Typography>
+
+                        <Typography
+                            sx={{ mb: 2, fontSize: { xs: "1rem", md: "17px" }, width: "95%" }}
+                        >
+                            {project.description}
+                        </Typography>
+
+                        <Box component="ul" sx={{ pl: 0, listStylePosition: "inside", mb: 3 }}>
+                            {project.points.map((p, i) => (
+                                <li key={i}>{p}</li>
+                            ))}
+                        </Box>
+
+                        <Stack direction="row" flexWrap="wrap" spacing={1} mb={3}>
+                            {project.tags.map((tag, i) => (
+                                <Chip
+                                    key={i}
+                                    label={tag}
+                                    sx={{
+                                        backgroundColor: "rgba(255,255,255,0.15)",
+                                        color: "white",
+                                        transition: "all 0.3s ease",
+                                        cursor: "pointer",
+
+                                        "&:hover": {
+                                            backgroundColor: "rgba(255,255,255,0.3)",
+                                            transform: "scale(1.05)",
+                                        },
+                                    }}
+                                />
+                            ))}
+                        </Stack>
+
+
+                        <Button
+                            variant="outlined"
+                            sx={{ color: "white", borderColor: "white", alignSelf: "flex-start" }}
+                        >
+                            View Case Study
+                        </Button>
+                    </Box>
+
+                    <Box
+                        component="img"
+                        src={project.overlayImage}
+                        alt="overlay-mobile"
+                        sx={{
+                            display: { xs: "block", sm: "block", md: "none" },
+                            width: { xs: "140px", sm: "160px" },
+                            marginTop: "10px",
+                            marginBottom: "10px",
+                            alignSelf: "center",
+                        }}
+                    />
+                </Box>
+            </Container>
 
             <Box
                 component="img"
                 src={project.overlayImage}
-                alt={`${project.projectTitle} overlay`}
+                alt="overlay-image"
                 sx={{
+                    position: { xs: "static", md: "absolute" },
+                    top: { md: "54%" },
+                    ...(isRight
+                        ? {
+                            right: { md: "44%", lg: "47%" },
+                            transform: { md: "translate(50%, -50%)" },
+                        }
+                        : {
+                            left: { md: "47%" },
+                            transform: { md: "translate(-50%, -50%)" },
+                        }),
+                    width: {
+                        xs: "0px",
+                        md: "150px",
+                        lg: "170px",
+                    },
                     display: { xs: "none", md: "block" },
-                    position: "absolute",
-                    top: "60%",
-                    left:
-                        direction === "image-right"
-                            ? { xs: "50%", sm: "43%", md: "43%", lg: "47%" }
-                            : { xs: "50%", sm: "52%", md: "59%", lg: "55%", xl: "53.5%" },
-                    transform: "translate(-55%, -56%)",
-                    width: { md: "200px" },
-                    height: "auto",
                     zIndex: 3,
-                    transition: "all 0.3s ease",
+                    mb: { xs: 0, md: "40px" },
                 }}
             />
         </Box>
